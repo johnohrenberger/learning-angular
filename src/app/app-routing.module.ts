2e61099f902bbba547a03ed813265bdc36d05201
John Ohrenberger <johren.dev@gmail.com>
2020-10-17 14:40:28 -0400

    Adding Child Routing to Homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  { path: 'recipes', component: RecipesComponent },
+  { path: 'recipes', component: RecipesComponent, children: [
+    { path: '', component: RecipesStartComponent },
+    { path: ':id', component: RecipeDetailComponent }
+  ]},
   { path: 'shopping-list', component: ShoppingListComponent }
 ]
 
